b99dfa5f9ea058e450e09227c259984d23c7fea0
Adam Pearce <1wheel@gmail.com>
2016-11-13 12:47:00 -0500

    version one

--- README.md
+++ README.md
@@ -12,23 +12,10 @@ Then, navigate to the directory with your gist and run:
 
     gist-snap
 
-This takes creates a `preview.png` and `thumbnail.png` of your `index.html` and commits/pushes them. 
-
-gist-snap is conservative - if you don't have an `index.html` in your folder, if the images files already exist, or if your local files aren't in sync with origin/master, it will exit without modifying anything. 
-
-If you've set up your bl.ocks to share common files, like [Let's Make a Block](https://bost.ocks.org/mike/block/) suggests, pass your user name to take a screen shot of bl.ocks directly:  
-
-    gist-snap --user 1wheel
-
-If you've got all of your gists in one folder, you can add images to all of them in one go with: 
-
-    for d in ./*/ ; do (cd "$d" && printf '\n%s\n' "${PWD##*/}" && gist-snap); done
-
+This takes creates a `preview.png` and `thumbnail.png` of your `index.html`, overwriting any existing images. 
 
 Options: 
 
-    --overwrite - overwrites existing image files
-    --nocommit - doesn't commit or push to remote
     --delay <delay> - milliseconds to wait before screenshot
 
 # More scripts
@@ -50,3 +37,7 @@ To clone and rename:
     var remote = 'git@gist.github.com:' + id + '.git'
 
     copy(['git clone', remote, name, '&& cd', name, '&& gistup-rename', name, '&& cd ..'].join(' '))
+
+If you've got all of your gists in one folder, you can add images to all of them in one go with: 
+
+    for d in ./*/ ; do (cd "$d" && printf '\n%s\n' "${PWD##*/}" && gist-snap); done
--- index.js
+++ index.js
@@ -1,10 +1,8 @@
 #!/usr/bin/env node
 
-var glob = require('glob'),
-    express = require('express'),
+var express = require('express'),
     serveStatic = require('serve-static'),
     d3 = require('d3'),
-    child = require('child_process'),
     Nightmare = require('nightmare')
 
 var argv = require('minimist')(process.argv.slice(2))
@@ -13,59 +11,14 @@ var port = 3870
 var gistID = null
 
 d3.queue(1)
-  .defer(fileCheck)
-  .defer(gitCheck)
   .defer(initServer)
   .defer(snapPreview)
   .defer(resizeThumb)
-  .defer(gitUpload)
   .awaitAll(function(err, res){
     console.log(err || 'gist-snap finished')
 
     process.exit()
   })
-
-
-//directory should have index.html and no thumbnail.png or preview.png
-function fileCheck(cb){
-  glob('*', {}, function(err, files){
-    if (!err && !~files.indexOf('index.html'))    err = 'no index.html'
-
-    //don't check for existing images if overwrite is on
-    if (argv.overwrite) return cb(err)
-
-    if (!err &&  ~files.indexOf('thumbnail.png'))
-      err = 'thumbnail.png present; to modify, run gist-snap --overwrite' 
-    if (!err &&  ~files.indexOf('preview.png'))
-      err = 'preview.png present; to modify, run gist-snap --overwrite' 
-    cb(err)
-  })
-}
-
-
-//make sure that working directory is in sync with origin/master
-function gitCheck(cb){
-  if (argv.nocommit) return cb()
-
-  child.exec('git version && git fetch && git status && git remote -v', function(err, stdout, stderr){
-    if (!err && stderr) 
-      err = stderr
-    if (!err && !~stdout.indexOf('git version 2'))
-      err = 'gist-snap requires git version 2'
-    if (!err && !~stdout.indexOf('On branch master'))
-      err = 'not on branch master'
-    if (!err && !~stdout.indexOf("Your branch is up-to-date with 'origin/master'."))
-      err = 'not synced with origin/master'
-    if (!err && !~stdout.indexOf('nothing to commit, working directory clean'))
-      err = 'uncommited local changes'
-
-    if (!stderr) gistID = stdout.split('git@gist.github.com:')[1].split('.git')[0]
-    if (err) err = err + '\n' + 'to skip making a new commit, run gist-snap --nocommit'
-
-    cb(err)
-  })
-}
-
 
 //statically serve directory
 function initServer(cb){
@@ -76,21 +29,17 @@ function initServer(cb){
   cb(null) 
 }
 
-
-
+//screen shot index.html
 function snapPreview(cb){
-  var url = argv.user ? 
-    `http://bl.ocks.org/${argv.user}/raw/${gistID}` : 
-    `http://localhost:${port}/index.html`
-
   new Nightmare()
     .viewport(960, 500)
-    .goto(url)
+    .goto(`http://localhost:${port}/index.html`)
+    .wait(argv.delay || 3000)
     .screenshot('preview.png')
     .run(cb)
 }
 
-
+//generate thumbnail
 function resizeThumb(cb){
   require('lwip').open('preview.png', function(err, image){
     if (err) cb(err)
@@ -98,15 +47,5 @@ function resizeThumb(cb){
     image.batch()
       .resize(230, 120)
       .writeFile('thumbnail.png', cb)
-  })
-}
-
-
-function gitUpload(cb){
-  if (argv.nocommit) return cb()
-
-  child.exec('git add thumbnail.png preview.png && git commit -m "adds preview images with gist-snap" && git push', function(err, stdout, stderr){
-    if (!err && stderr) err = stderr
-    cb(err)
   })
 }
